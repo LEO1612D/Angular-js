a1e0bce906a476f7dc7f5460d9ed7dc9e727fb7e
LEO1612D <46849277+LEO1612D@users.noreply.github.com>
2020-10-13 15:59:11 +0530

    up-to-filter

--- L-11/app.js
+++ L-11/app.js
@@ -1,17 +1,46 @@
 (() => {
   "use strict";
 
-  const MsgController = $scope => {
+  const MsgController = ($scope, $filter, lovesFilter) => {
     $scope.name = "nikunj";
     $scope.color = "red";
 
-    $scope.sayMessage = () => "Nikunj likes react js";
+    $scope.cookieCost = 0.45;
+
+    $scope.sayMessage = () => {
+      const msg = "Nikunj likes react js and python ";
+      const output = $filter("uppercase")(msg);
+      return output;
+    };
+
+    $scope.sayLoves = () => {
+      const msg = "Nikunj likes react js and python ";
+      const lmsg = lovesFilter(msg);
+      return lmsg;
+    };
 
     $scope.choose = () => {
       $scope.color = "blue";
     };
   };
 
-  MsgController.$inject = ["$scope"];
-  angular.module("MsgApp", []).controller("MsgController", MsgController);
+  // CUSTOM FILTER----------------------------------
+  const LovesFilter = () => input => {
+    input = input || "";
+    input = input.replace("likes", "loves");
+    return input;
+  };
+
+  const TruthFilter = () => (input, target, replace) => {
+    input = input || "";
+    input = input.replace(target, replace);
+    return input;
+  };
+
+  MsgController.$inject = ["$scope", "$filter", "lovesFilter"];
+  angular
+    .module("MsgApp", [])
+    .controller("MsgController", MsgController)
+    .filter("loves", LovesFilter)
+    .filter("truth", TruthFilter);
 })();
